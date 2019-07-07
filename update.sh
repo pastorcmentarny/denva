00c279ef4b4119160144a6b2a9e8a057f270a604
Dominik Symonowicz <dmspastor@gmail.com>
2019-07-07 13:58:49 +0100

    update script

--- update.sh
+++ update.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-branch $1
-echo "re-run project using branch " + branch
+echo "re-run project using branch $1"
 cd /home/pi
-rm branch + ".zip"
-rm -rf "denva-" + branch
-wget "https://github.com/pastorcmentarny/denva/archive/" + branch + ".zip"
-unzip branch + ".zip"
-cd "denva-" + branch + "/src/"
+rm "$1.zip"
+rm -rf "denva-$1"
+wget "https://github.com/pastorcmentarny/denva/archive/$1.zip"
+unzip "$1.zip"
+cd "denva-$1/src/"
 sudo python3 app.py
