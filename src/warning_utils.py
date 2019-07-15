a88012d3b5471840c4d1355cf5c9f8176d9661ad
Dominik Symonowicz <dmspastor@gmail.com>
2019-07-15 18:41:23 +0100

    fix  warns now

--- src/warning_utils.py
+++ src/warning_utils.py
@@ -1,6 +1,9 @@
 import re
+
+
 def get_warnings(data) -> dict:
     warnings = {}
+    data['temp'] = float(data['temp'])
     if data['temp'] < 16:
         warnings['temp'] = 'Temperature is too low. Current temperature is: ' + str(data['temp'])
     elif data['temp'] < 18:
@@ -9,6 +12,8 @@ def get_warnings(data) -> dict:
         warnings['temp'] = 'Temperature is high. Current temperature is: ' + str(data['temp'])
     elif data['temp'] > 30:
         warnings['temp'] = 'Temperature is too high. Current temperature is: ' + str(data['temp'])
+
+    data['humidity'] = float(data['humidity'])
 
     if data['humidity'] < 30:
         warnings['humidity'] = 'Humidity is too low. Current humidity is: ' + str(data['humidity'])
@@ -19,13 +24,17 @@ def get_warnings(data) -> dict:
     elif data['humidity'] > 70:
         warnings['humidity'] = 'Humidity is too high. Current humidity is: ' + str(data['humidity'])
 
+    data['uva_index'] = float(data['uva_index'])
+
     if data['uva_index'] > 6:
         warnings['uva_index'] = 'UV A is too high. Current UV A is: ' + str(data["uva_index"])
+
+    data['uvb_index'] = float(data['uvb_index'])
 
     if data['uvb_index'] > 6:
         warnings['uvb_index'] = 'UV B is too high. Current UV B is: ' + str(data["uvb_index"])
 
-    data['cpu_temp'] = re.sub('[^0-9.]', '', data['cpu_temp'])
+    data['cpu_temp'] = float(re.sub('[^0-9.]', '', data['cpu_temp']))
 
     if data['cpu_temp'] > 75:
         warnings['cpu_temp'] = 'CPU temperature is too high. Current temperature is: ' + str(data['cpu_temp'])
