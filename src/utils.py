1583e4318a78d3540bfb6c014bd0133e4d706a9e
Dominik Symonowicz <dmspastor@gmail.com>
2019-07-28 23:08:22 +0100

    add get_yesterday_date to utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,11 @@
 import datetime
+from datetime import timedelta
 
 
 def get_date_as_filename(name: str, type: str, dt: datetime) -> str:
     return f"{name}-{dt.year}-{dt.month:02d}-{dt.day:02d}.{type}"
+
+
+def get_yesterday_date() -> datetime:
+    today = datetime.datetime.now()
+    return today - timedelta(days=1)
