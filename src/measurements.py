773b09a6208ed453192c83c640792a794ddb6a7d
Dominik Symonowicz <dmspastor@gmail.com>
2019-07-28 17:14:28 +0100

    remove logging measurement as i log them in different file

--- src/measurements.py
+++ src/measurements.py
@@ -3,29 +3,12 @@
 
 import csv
 import datetime
-import logging
 
 import get_description_for
 import sensor_log_reader
 
-logger = logging.getLogger('app')
-
 
 def store(data, motion):
-    measurement = 'temp: {} pressure: {} humidity: {} gas_resistance {}, colour: {} AQI: {} UVA: {} UVB: {} motion: {} motion diff: {}'.format(
-        data['temp'],
-        data['pressure'],
-        data['humidity'],
-        data['gas_resistance'],
-        data['colour'],
-        data['aqi'],
-        data['uva_index'],
-        data['uvb_index'],
-        data['motion'],
-        get_description_for.get_motion_as_string(motion),
-        data["cpu_temp"],
-        str(data['measurement_time']) + ' ms.')
-    logger.info(measurement)
     timestamp = datetime.datetime.now()
     sensor_log_file = open(sensor_log_reader.get_sensor_log_file(), 'a+', newline='')
     csv_writer = csv.writer(sensor_log_file)
