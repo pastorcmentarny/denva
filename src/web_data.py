b4ba9f97c7ded430cee5367b380deef2fc4c8fa9
Dominik Symonowicz <dmspastor@gmail.com>
2019-08-01 23:03:27 +0100

    add try/expect to handle problem

--- src/web_data.py
+++ src/web_data.py
@@ -4,30 +4,35 @@ import requests
 
 
 def get_train() -> str:
-    response = requests.get('https://www.nationalrail.co.uk/service_disruptions/indicator.aspx')
-    train_status = ''
     try:
+        response = requests.get('https://www.nationalrail.co.uk/service_disruptions/indicator.aspx')
+        train_status = ''
         response.raise_for_status()  # without try it will exit program
         html_manager = bs4.BeautifulSoup(response.text, "html.parser")
 
         response = html_manager.select('tr.accordian-header')
-        x = response[2].find_all('td')
-        train_status += x[0].text.replace(' Railways', '') + ': ' + x[1].text
+        train_tag = response[2].find_all('td')
+        train_status += train_tag[0].text.replace(' Railways', '') + ': ' + train_tag[1].text
     except Exception as whoops:
-        print('Unable to get weather temperature due to : %s' % whoops)
+        print('Unable to get train data due to : %s' % whoops)
+        train_status = 'Train data N/A'
     return train_status
 
 
 def get_tube():
-    response = requests.get('https://api.tfl.gov.uk/line/mode/tube/status')
-    data = json.loads(response.text)
-    tubes = []
-    for i in data:
-        if i['id'] == 'metropolitan' or i['id'] == 'jubilee' or i['id'] == 'victoria':
-            text = i['id'].capitalize() + ': '
-            for status in i['lineStatuses']:
-                text += status['statusSeverityDescription']
-            tubes.append(text)
+    try:
+        response = requests.get('https://api.tfl.gov.uk/line/mode/tube/status')
+        data = json.loads(response.text)
+        tubes = []
+        for i in data:
+            if i['id'] == 'metropolitan' or i['id'] == 'jubilee' or i['id'] == 'victoria':
+                text = i['id'].capitalize() + ': '
+                for status in i['lineStatuses']:
+                    text += status['statusSeverityDescription']
+                tubes.append(text)
+    except Exception as whoops:
+        print('Unable to get tube data due to : %s' % whoops)
+        tubes = ['Tube data N/A']
     return tubes
 
 
