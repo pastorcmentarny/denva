f8d41e49f972c1b776d401fe847f234f72665072
dominik symonowicz <dmspastor@gmail.com>
2020-01-02 21:21:07 +0000

    fix typo in path

--- scripts/update_server_locally.py
+++ scripts/update_server_locally.py
@@ -2,4 +2,5 @@ from distutils.dir_util import copy_tree
 
 '''this script is used to copy project locally to my server'''
 
-copy_tree("D:\\Projects\\denva\\src", "\\ds-lpd-server\\denva\\src")
+result = copy_tree("D:\\Projects\\denva\\src", "\\ds-lpd-server\\denva\\src")
+print(result)
