29b774a2302d624698d5d6de83ef08048c77f69c
dominik symonowicz <dmspastor@gmail.com>
2020-01-05 13:14:14 +0000

    fix config for logs

--- src/app_server.py
+++ src/app_server.py
@@ -9,31 +9,47 @@
 * Google Play:	https://play.google.com/store/apps/developer?id=Dominik+Symonowicz
 * LinkedIn: https://www.linkedin.com/in/dominik-symonowicz
 """
+from timeit import default_timer as timer
 import logging
 import sys
 import time
-
-import commands
 import data_files
 import email_sender_service
+import commands
 
 logger = logging.getLogger('app')
 
+pictures = []
 
 def main():
-    logger.info('Starting taking picture thread..', exc_info=True)
+    counter = 0;
     while True:
+        counter+=1
         time.sleep(5)
         last_picture = commands.capture_picture()
+
         if last_picture != "":
             pictures.append(last_picture)
             if len(pictures) > 5:
                 pictures.pop(0)
+        print(pictures)
+
+
+def info(msg:str):
+    logger.info(msg)
+    print(msg)
+
+
+def setup():
+    start_time = timer()
+    data_files.setup_logging('server')
+    end_time = timer()
+    info('Setup took {} ms.'.format(int((end_time - start_time) * 1000)))
 
 
 if __name__ == '__main__':
     print('Starting Server App ... \n Press Ctrl+C to shutdown')
-    data_files.setup_logging()
+    setup()
     try:
         main()
     except Exception as e:
