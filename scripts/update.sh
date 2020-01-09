b6242615d498c9da42694e9e2c44905d5273d7b7
dominik symonowicz <dmspastor@gmail.com>
2020-01-09 22:03:21 +0000

    update update.sh script, so check for both parameters present when run script

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,10 @@
 
 if [[ -z "$1" ]]
 then
-      echo "\$var is empty"
+      echo "No params provided. You need 2 params branch and app to run. For example: sudo bash master app"
+elif [[ -z "$2" ]]
+then
+      echo "only 1 param provided. You need 2 params branch and app to run. For example: sudo bash master app"
 else
     echo "re-run project using branch $1"
     cd /home/pi
