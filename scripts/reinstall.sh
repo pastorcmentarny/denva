e0b33e53b8756b0ab137bc800ff44805b210d98b
dominik symonowicz <dmspastor@gmail.com>
2020-01-09 22:09:39 +0000

    rename script to reinstall as its purpose was to reinstall app and restart Pi. implement script

---
+++ scripts/reinstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#this task is to update to newest version and reboot
+
+branch = "master"
+
+#!/bin/bash
+    echo "killing app and server"
+    sudo kill $(ps aux | grep '[p]ython3' | awk '{print $2}' | xargs sudo kill -15 )
+    echo "removing existing apps"
+    cd /home/pi
+    rm "$branch.zip"
+    rm -rf "denva-$branch"
+    echo "installing new version from github"
+    wget "https://github.com/pastorcmentarny/denva/archive/$1.zip"
+    unzip "$1.zip"
+    echo "rebooting Pi"
+    sudo reboot
