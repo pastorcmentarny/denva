096348697e3ccc1304b0f0bd07c477dd52ecaeac
dominik symonowicz <dmspastor@gmail.com>
2020-02-12 11:25:05 +0000

    add example of sentnce so i can practice beard, bear , bird and beer.

--- src/mothership/good_english_sentence.py
+++ src/mothership/good_english_sentence.py
@@ -5,7 +5,8 @@ sentences = [
     'This has been caused by an issue with the signalling system at Baker Street.',
     "Michael Jackson was taking propofol to sleep, which is like doing chemotherapy because you're tired of shaving your head. (Robbie Williams)",
     "Brent Cross Shopping Centre is hell on the earth on saturday.",
-    "Jeremy Clarskon - '(...) to create a vision of pure, what's the word? May and Hammond 'RUBBISH!"
+    "Jeremy Clarskon - '(...) to create a vision of pure, what's the word? May and Hammond 'RUBBISH!",
+    "(4B) I shaved off my beard and I went to 'bear & bird' pub to drink a few pints of beer."
 ]
 
 
