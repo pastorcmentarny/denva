3fba45412bcf6619b1bacad62b285636739fb0a2
dominik symonowicz <dmspastor@gmail.com>
2020-02-12 11:23:32 +0000

    add a few sentence and 1 quote by Jeremy Clarkson

--- src/mothership/good_english_sentence.py
+++ src/mothership/good_english_sentence.py
@@ -2,9 +2,12 @@ import random
 
 sentences = [
     'I am extremely sorry for the disruption that Metropolitan line customers are experiencing this morning.'
-    'This has been caused by an issue with the signalling system at Baker Street.'
+    'This has been caused by an issue with the signalling system at Baker Street.',
+    "Michael Jackson was taking propofol to sleep, which is like doing chemotherapy because you're tired of shaving your head. (Robbie Williams)",
+    "Brent Cross Shopping Centre is hell on the earth on saturday.",
+    "Jeremy Clarskon - '(...) to create a vision of pure, what's the word? May and Hammond 'RUBBISH!"
 ]
 
 
 def get_random_english_sentence() -> str:
-    return sentences[random.randint(0, len(sentences) - 1)]
+    return sentences[random.randint(0, len(sentences))]
