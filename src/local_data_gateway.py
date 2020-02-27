d5c7615221af68f669e6833df77f04f54510de7a
pastor cmentarny <dmspastor@gmail.com>
2020-02-27 21:59:23 +0000

    add prototype for get reading for denva and enviro and use by server

--- src/local_data_gateway.py
+++ src/local_data_gateway.py
@@ -2,6 +2,14 @@ import json
 import requests
 
 
+def get_current_reading_for_denva() -> dict:
+    return get_data_for('http://192.168.0.2:5000/now')
+
+
+def get_current_reading_for_enviro() -> dict:
+    return get_data_for('http://192.168.0.4:5000/now')
+
+
 def get_data_for(url: str) -> dict:
     response = requests.get(url)
     try:
@@ -23,3 +31,6 @@ return {
     'enviro' : get_data_for(config_serivce.get_current_warnings_url__for('enviro'))
 }
 '''
+
+if __name__ == '__main__':
+    print(get_current_reading_for_denva())
