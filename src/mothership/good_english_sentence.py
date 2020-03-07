c4399298073358867c3e86d3fca979e022a07cc4
dominik symonowicz <dmspastor@gmail.com>
2020-03-07 12:15:44 +0000

    remove sentence

--- src/mothership/good_english_sentence.py
+++ src/mothership/good_english_sentence.py
@@ -7,7 +7,6 @@ sentences = [
     "Brent Cross Shopping Centre is hell on the earth on saturday.",
     "Jeremy Clarskon - '(...) to create a vision of pure, what's the word? May and Hammond 'RUBBISH!",
     "(4B) I shaved off my beard and I went to 'bear & bird' pub to drink a few pints of beer.",
-    "Though, Thought, Tough, Through and Thorough are most confusing English words for me. Tough is an adjective and it means hard, difficult, trick. Throw” =  verb= To move something in the a",
     "I do not have knowledge on that matter.",
     "There've been a few developments."
 ]
