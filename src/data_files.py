611266cd34dae0fddaeb20f02fff9fd315d94a23
dominik symonowicz <dmspastor@gmail.com>
2020-03-12 20:09:49 +0000

    fix load config for dev server denva and enviro

--- src/data_files.py
+++ src/data_files.py
@@ -25,8 +25,10 @@ logger = logging.getLogger('app')
 
 
 def load_cfg() -> dict:
-    if     config_serivce.load_cfg()['mode']:
+    if config_serivce.load_cfg()['mode'] == 'dev':
         path = 'd:\denva\email.json'
+    elif config_serivce.load_cfg()['mode'] == 'server':
+        path = 'e:\denva\email.json'
     else:
         path = '/home/pi/email.json'  # actual cfg is different place
     with open(path, 'r') as email_config:
