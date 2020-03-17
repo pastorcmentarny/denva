7fbd557cbbf91a5125e782813724ea266fd1e26e
dominik symonowicz <dmspastor@gmail.com>
2020-03-17 06:56:18 +0000

    add get system information

--- src/system_data_service.py
+++ src/system_data_service.py
@@ -1,5 +1,27 @@
-import config_serivce
 import psutil
+from _datetime import datetime
+
+import config_serivce
+import utils
+
+
+def get_boot_time() -> str:
+    return datetime.fromtimestamp(psutil.boot_time()).strftime("%d-%m'%Y @ %H:%M:%S")
+
+
+def get_system_disk_space_free():
+    return utils.convert_bytes_to_MB(psutil.disk_usage(config_serivce.get_system_drive()).free)
+
+
+# add disk free
+def get_system_information() -> dict:
+    return {
+        "CPU freq": psutil.cpu_freq().current,
+        "Memory": utils.convert_bytes_to_MB(psutil.virtual_memory().available),
+        "Disk Free": get_system_disk_space_free(),
+        "Boot Time": get_boot_time()
+
+    }
 
 
 def get_system_warnings() -> list:
@@ -10,5 +32,7 @@ def get_system_warnings() -> list:
         problems.append('Memory available is low. Memory left: {} bytes'.format(memory_data.available))
     return problems
 
+
 if __name__ == '__main__':
+    print(get_system_information())
     print(get_system_warnings())
