44d6d0562b46d539520726813f43038a821676ad
dominik symonowicz <dmspastor@gmail.com>
2020-04-07 11:16:53 +0100

    extract display in denviro into external file

--- src/app_enviro.py
+++ src/app_enviro.py
@@ -10,10 +10,8 @@
 * LinkedIn: https://www.linkedin.com/in/dominik-symonowicz
 """
 
-import os
 from timeit import default_timer as timer
 
-import ST7735
 import sys
 import time
 
@@ -32,9 +30,7 @@ from bme280 import BME280
 from pms5003 import PMS5003, ReadTimeoutError as pmsReadTimeoutError
 from enviroplus import gas
 from subprocess import PIPE, Popen
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+
 import logging
 import random
 import re
@@ -42,6 +38,7 @@ import cl_display
 import commands
 import config_service
 import data_files
+from denviro import denviro_display
 import email_sender_service
 import sensor_warnings
 import system_data_service
@@ -50,23 +47,7 @@ logger = logging.getLogger('app')
 
 bme280 = BME280()
 pms5003 = PMS5003()
-st7735 = ST7735.ST7735(
-    port=0,
-    cs=1,
-    dc=9,
-    backlight=12,
-    rotation=270,
-    spi_speed_hz=10000000
-)
-st7735.begin()
 
-# setup
-WIDTH = st7735.width
-HEIGHT = st7735.height
-img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
-draw = ImageDraw.Draw(img)
-path = os.path.dirname(os.path.realpath(__file__))
-font = ImageFont.truetype(path + "/fonts/Roboto-Regular.ttf", 14)
 message = ""
 top_pos = 25
 factor = 0.8
@@ -78,6 +59,7 @@ values = {}
 temps = []
 cpu_temps = []
 cycle = 0
+on = True
 
 
 def get_cpu_temperature() -> float:
@@ -148,7 +130,7 @@ def ui(msg: str, screen: bool = True):
     logger.info(msg)
     print(msg)
     if screen:
-        draw_message(msg)
+        denviro_display.draw_message(msg)
 
 
 def setup():
@@ -167,102 +149,6 @@ def setup():
     ui('It took {} ms.'.format(int((end_time - start_time) * 1000)))
 
 
-def display_on_screen(measurement: dict):
-    global cycle
-    draw.rectangle((0, 0, 160, 80), fill="black")
-    color1 = ['', random.randrange(0, 255, 1), random.randrange(0, 255, 1), random.randrange(0, 255, 1)]
-    color2 = ['', random.randrange(0, 255, 1), random.randrange(0, 255, 1), random.randrange(0, 255, 1)]
-    color3 = ['', random.randrange(0, 255, 1), random.randrange(0, 255, 1), random.randrange(0, 255, 1)]
-    line3 = ''
-
-    if cycle % 6 == 0:
-        line1 = 'CPU Temp: {}'.format(commands.get_cpu_temp())
-        line2 = 'IP: {}'.format(commands.get_ip())
-        line3 = 'Uptime: {}'.format(commands.get_uptime())
-    elif cycle % 6 == 1:
-        line1 = 'RAM avail.: {}'.format(system_data_service.get_memory_available_in_mb())
-        line2 = 'Space: {}'.format(commands.get_space_available())
-        line3 = 'Data Space: {}'.format(commands.get_data_space_available())
-
-        color1 = get_colour_for_cpu()
-
-    elif cycle % 6 == 2:
-        line1 = 'light: {}'.format(measurement["light"])
-        line2 = 'proximity: {}'.format(measurement["proximity"])
-    elif cycle % 6 == 3:
-        line1 = 'nh   3: {:.2f}'.format(measurement["nh3"])
-        line2 = 'oxidised: {:.2f}'.format(measurement["oxidised"])
-        line3 = 'reduced: {:.2f}'.format(measurement["reduced"])
-    else:
-        line1 = 'pm    1: {}'.format(measurement["pm1"])
-        line2 = 'pm 2.5: {}'.format(measurement["pm25"])
-        line3 = 'pm  10: {}'.format(measurement["pm10"])
-
-        color1 = get_colour(measurement["pm1"])
-        color2 = get_colour(measurement["pm25"])
-        color3 = get_colour(measurement["pm10"])
-
-    draw.text((0, 0), line1, font=font, fill=(color1[1], color1[2], color1[3]))
-    draw.text((0, 16), line2, font=font, fill=(color2[1], color2[2], color2[3]))
-    draw.text((0, 32), line3, font=font, fill=(color3[1], color3[2], color2[3]))
-    #shade_of_grey = random.randrange(128, 255, 1)
-    #draw.text((0, 48), line4, font=font, fill=(shade_of_grey, shade_of_grey, shade_of_grey))
-    st7735.display(img)
-    cycle += 1
-
-
-def get_colour_for_cpu():
-    cpu_temp = float(re.sub('[^0-9.]', '', commands.get_cpu_temp()))
-    config = config_service.load_cfg()
-    if cpu_temp > config['sensor']['cpu_temp_fatal']:
-        return ['Fatal', 255, 16, 1]
-    elif cpu_temp > config['sensor']['cpu_temp_error']:
-        return ['Fatal', 246, 108, 8]
-    elif cpu_temp > config['sensor']['cpu_temp_warn']:
-        return ['Warn', 255, 215, 0]
-    else:
-        return ['Good', 106, 168, 79]
-
-
-def get_colour(level: float) -> list:
-    if level < 15.5:
-        return ['Good', 0, 204, 0]
-    elif level < 40.5:
-        return ['Moderate', 255, 234, 0]
-    elif level < 65.5:
-        return ['Unhealthy for Sensitive', 255, 165, 0]
-    elif level < 150.5:
-        return ['Unhealthy', 255, 37, 0]
-    elif level < 250.5:
-        return ['Very Unhealthy', 165, 0, 255]
-    elif level < 500.5:
-        return ['Hazardous', 116, 0, 179]
-    else:
-        return ['Deadly?', 30, 30, 30]
-
-
-def draw_message(msg: str):
-    draw.rectangle((0, 0, 160, 80), fill="black")
-    shade_of_grey = random.randrange(127, 255, 1)
-    draw.text((0, 0), msg, font=font, fill=(shade_of_grey, shade_of_grey, shade_of_grey))
-    st7735.display(img)
-
-
-on = True
-
-
-def set_brightness_for_screen(proximity):
-    global on
-    if proximity > 1000:
-        on = not on
-        if on:
-            logger.info('switching ON backlight')
-            st7735.set_backlight(12)
-        else:
-            logger.info('switching OFF backlight')
-            st7735.set_backlight(0)
-
-
 def main():
     measurement_counter = 0
     setup()
@@ -272,14 +158,14 @@ def main():
 
         start_time = timer()
         measurement = get_measurement()
-        display_on_screen(measurement)
+        denviro_display.display_on_screen(measurement)
         measurement['cpu_temp'] = commands.get_cpu_temp()
         end_time = timer()
         measurement_time = str(int((end_time - start_time) * 1000))  # in ms
         measurement['measurement_time'] = measurement_time
         logger.info('it took ' + str(measurement_time) + ' milliseconds to measure it.')
         cl_display.print_measurement(measurement)
-        set_brightness_for_screen(measurement['proximity'])
+        denviro_display.set_brightness_for_screen(measurement['proximity'])
         data_files.store_enviro_measurement(measurement)
         measurement_storage_service.send('enviro', measurement)
         sensor_warnings.get_current_warnings_for_enviro()
@@ -302,6 +188,6 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt as keyboard_exception:
         logger.error('Something went badly wrong\n{}'.format(keyboard_exception), exc_info=True)
-        draw_message('APP crashed.')
+        denviro_display.draw_message('APP crashed.')
         sys.exit(0)
-    draw_message('Goodbye.')
+    denviro_display.draw_message('Goodbye.')
---
+++ src/denviro/denviro_display.py
@@ -0,0 +1,124 @@
+import config_service
+import logging
+import os
+import random
+import re
+
+import ST7735
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import commands
+import  system_data_service
+
+logger = logging.getLogger('app')
+
+st7735 = ST7735.ST7735(
+    port=0,
+    cs=1,
+    dc=9,
+    backlight=12,
+    rotation=270,
+    spi_speed_hz=10000000
+)
+st7735.begin()
+
+# setup
+path = os.path.dirname(os.path.realpath(__file__))
+WIDTH = st7735.width
+HEIGHT = st7735.height
+img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+draw = ImageDraw.Draw(img)
+font = ImageFont.truetype(path + "/fonts/Roboto-Regular.ttf", 14)
+
+def display_on_screen(measurement: dict):
+    global cycle
+    draw.rectangle((0, 0, 160, 80), fill="black")
+    color1 = ['', random.randrange(0, 255, 1), random.randrange(0, 255, 1), random.randrange(0, 255, 1)]
+    color2 = ['', random.randrange(0, 255, 1), random.randrange(0, 255, 1), random.randrange(0, 255, 1)]
+    color3 = ['', random.randrange(0, 255, 1), random.randrange(0, 255, 1), random.randrange(0, 255, 1)]
+    line3 = ''
+
+    if cycle % 6 == 0:
+        line1 = 'CPU Temp: {}'.format(commands.get_cpu_temp())
+        line2 = 'IP: {}'.format(commands.get_ip())
+        line3 = 'Uptime: {}'.format(commands.get_uptime())
+    elif cycle % 6 == 1:
+        line1 = 'RAM avail.: {}'.format(system_data_service.get_memory_available_in_mb())
+        line2 = 'Space: {}'.format(commands.get_space_available())
+        line3 = 'Data Space: {}'.format(commands.get_data_space_available())
+
+        color1 = get_colour_for_cpu()
+
+    elif cycle % 6 == 2:
+        line1 = 'light: {}'.format(measurement["light"])
+        line2 = 'proximity: {}'.format(measurement["proximity"])
+    elif cycle % 6 == 3:
+        line1 = 'nh   3: {:.2f}'.format(measurement["nh3"])
+        line2 = 'oxidised: {:.2f}'.format(measurement["oxidised"])
+        line3 = 'reduced: {:.2f}'.format(measurement["reduced"])
+    else:
+        line1 = 'pm    1: {}'.format(measurement["pm1"])
+        line2 = 'pm 2.5: {}'.format(measurement["pm25"])
+        line3 = 'pm  10: {}'.format(measurement["pm10"])
+
+        color1 = get_colour(measurement["pm1"])
+        color2 = get_colour(measurement["pm25"])
+        color3 = get_colour(measurement["pm10"])
+
+    draw.text((0, 0), line1, font=font, fill=(color1[1], color1[2], color1[3]))
+    draw.text((0, 16), line2, font=font, fill=(color2[1], color2[2], color2[3]))
+    draw.text((0, 32), line3, font=font, fill=(color3[1], color3[2], color2[3]))
+    #shade_of_grey = random.randrange(128, 255, 1)
+    #draw.text((0, 48), line4, font=font, fill=(shade_of_grey, shade_of_grey, shade_of_grey))
+    st7735.display(img)
+    cycle += 1
+
+def set_brightness_for_screen(proximity):
+    global on
+    if proximity > 1000:
+        on = not on
+        if on:
+            logger.info('switching ON backlight')
+            st7735.set_backlight(12)
+        else:
+            logger.info('switching OFF backlight')
+            st7735.set_backlight(0)
+
+def draw_message(msg: str):
+    draw.rectangle((0, 0, 160, 80), fill="black")
+    shade_of_grey = random.randrange(127, 255, 1)
+    draw.text((0, 0), msg, font=font, fill=(shade_of_grey, shade_of_grey, shade_of_grey))
+    st7735.display(img)
+
+
+def get_colour_for_cpu():
+    cpu_temp = float(re.sub('[^0-9.]', '', commands.get_cpu_temp()))
+    config = config_service.load_cfg()
+    if cpu_temp > config['sensor']['cpu_temp_fatal']:
+        return ['Fatal', 255, 16, 1]
+    elif cpu_temp > config['sensor']['cpu_temp_error']:
+        return ['Fatal', 246, 108, 8]
+    elif cpu_temp > config['sensor']['cpu_temp_warn']:
+        return ['Warn', 255, 215, 0]
+    else:
+        return ['Good', 106, 168, 79]
+
+
+def get_colour(level: float) -> list:
+    if level < 15.5:
+        return ['Good', 0, 204, 0]
+    elif level < 40.5:
+        return ['Moderate', 255, 234, 0]
+    elif level < 65.5:
+        return ['Unhealthy for Sensitive', 255, 165, 0]
+    elif level < 150.5:
+        return ['Unhealthy', 255, 37, 0]
+    elif level < 250.5:
+        return ['Very Unhealthy', 165, 0, 255]
+    elif level < 500.5:
+        return ['Hazardous', 116, 0, 179]
+    else:
+        return ['Deadly?', 30, 30, 30]
+
