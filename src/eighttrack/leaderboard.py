d3a536e58c79fe7733261cc8ab7abd944dee1f57
dominik symonowicz <dmspastor@gmail.com>
2020-04-07 17:05:46 +0100

    add get result by id

--- src/eighttrack/leaderboard.py
+++ src/eighttrack/leaderboard.py
@@ -2,29 +2,41 @@ results = [
     {
         'date': '1.4',
         'time': '24.46.3',
-        'lap': 1
+        'lap': 1,
+        'id': 3
     }, {
         'date': '1.4',
         'time': '21.38.8',
-        'lap': 2
+        'lap': 2,
+        'id': 4
     }, {
         'date': '31.3',
         'time': '26.21.9',
-        'lap': 1
+        'lap': 1,
+        'id': 2
     }, {
         'date': '30.3',
         'time': '30.53.1',
-        'lap': 1
+        'lap': 1,
+        'id': 1
     }, {
-        'date': '1.4',
+        'date': '6.4',
         'time': '24.46.3',
-        'lap': 1
+        'lap': 1,
+        'id': 5
     }, {
         'date': '7.4',
         'time': '25.21.9',
-        'lap': 1
+        'lap': 1,
+        'id': 6
     }
 ]
+
+def get_result_by_id(result_id:int) -> dict:
+    for result in results:
+        if result['id'] == result_id:
+            return result
+    return {}
 
 
 def load_results() -> list:
--- test/eighttrack/test_leaderboard.py
+++ test/eighttrack/test_leaderboard.py
@@ -2,14 +2,37 @@ import unittest
 
 from eighttrack import leaderboard
 
+
 class LeaderboardTestCase(unittest.TestCase):
     def test_load_results(self):
         # given
-        expected_first_result = {'date': '1.4', 'lap': 1, 'time': '24.46.3'}
+        expected_first_result = {'date': '1.4', 'lap': 1, 'time': '24.46.3', 'id': 3}
         # when
         result = leaderboard.load_results()
         # then
-        self.assertEqual(result[0],expected_first_result)
-        self.assertEqual(len(result),6)
+        self.assertEqual(result[0], expected_first_result)
+        self.assertEqual(len(result), 6)
 
+    def test_should_get_result_by_id(self):
+        # given
+        result_id = 6
+        expected_result = {
+            'date': '7.4',
+            'time': '25.21.9',
+            'lap': 1,
+            'id': result_id
+        }
+        # when
+        result = leaderboard.get_result_by_id(result_id)
 
+        # then
+        self.assertEqual(result, expected_result)
+
+    def test_should_return_None_if_id_do_not_match_when_get_result_by_id(self):
+        # given
+        result_id = 999
+        # when
+        result = leaderboard.get_result_by_id(result_id)
+
+        # then
+        self.assertEqual(result, {})
