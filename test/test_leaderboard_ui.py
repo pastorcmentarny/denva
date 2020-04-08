5ff66c7c1d0ba6f08e35472f9e3d25b6e62cb658
pastor cmentarny <dmspastor@gmail.com>
2020-04-08 17:03:02 +0100

    add generate highscore as text

---
+++ test/test_leaderboard_ui.py
@@ -0,0 +1,35 @@
+import io
+import sys
+import utils
+
+from unittest import TestCase
+from eighttrack import leaderboard_ui
+
+
+class LeaderboardUITest(TestCase):
+
+    def test_show_on_cli(self):
+        # given
+        date = utils.get_timestamp_title(False)
+        data = [{
+            'date': '31.3',
+            'time': '26.21.9',
+            'lap': 1,
+            'id': 2
+        }, {
+            'date': '30.3',
+            'time': '30.53.1',
+            'lap': 1,
+            'id': 1
+        }]
+        expected_result = """Current Highscore Table {}
+Id:2 @ 31.3 [Lap:1]...... 26.21.9 ds.
+Id:1 @ 30.3 [Lap:1]...... 30.53.1 ds.
+-----------------------------------------
+""".format(date)
+
+        # when
+        result = leaderboard_ui.generate_highscore_as_text(data)
+
+        # then
+        self.assertEqual(utils.to_multiline(result), expected_result)  # Now works as before.
