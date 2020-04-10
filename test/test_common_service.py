b28d92c64ca0e1e1bdc6bf943a4918f80047883d
dominik symonowicz <dmspastor@gmail.com>
2020-04-10 13:29:01 +0100

    more leaderboard cleanup

--- test/test_common_service.py
+++ test/test_common_service.py
@@ -1,20 +1,24 @@
 from unittest import TestCase
 
 import common_service
+import config_service
 import system_data_service
 import utils
 
 
 class Test(TestCase):
     def test_run_gc_in_server_service(self):
-        # given
-        memory_available_in_mb = system_data_service.get_memory_available_in_mb()
+        if config_service.run_slow_test():
+            # given
+            memory_available_in_mb = system_data_service.get_memory_available_in_mb()
 
-        # when
-        result = common_service.run_gc()
+            # when
+            result = common_service.run_gc()
 
-        # then
-        self.assertEqual(result['memory_before'], memory_available_in_mb)
-        self.assertLessEqual(utils.get_int_number_from_text(result['memory_after']),
-                             utils.get_int_number_from_text(memory_available_in_mb))
-        self.assertGreaterEqual(result['memory_saved'], 0)
+            # then
+            self.assertEqual(result['memory_before'], memory_available_in_mb)
+            self.assertLessEqual(utils.get_int_number_from_text(result['memory_after']),
+                                 utils.get_int_number_from_text(memory_available_in_mb))
+            self.assertGreaterEqual(result['memory_saved'], 0)
+        else:
+            self.skipTest('running fast test only. test_network_check_should_return_perfect skipped.')
--- test/test_leaderboard.py
+++ test/test_leaderboard.py
@@ -41,15 +41,9 @@ class LeaderboardTestCase(unittest.TestCase):
 
     def test_should_add_result(self):
         # given
-        id = len(leaderboard.results)+1
-        lap_result = '59.59.9--1.1--1'
-        expected_result = {
-            'date': '1.1',
-            'time': '59.59.9',
-            'time_in_ds': 35999,
-            'lap': 1,
-            'id': id
-        }
+        id = len(leaderboard.results) + 1
+        lap_result = '59.59.9--1.1.2068--1'
+        expected_result = config_test.get_result_with_id(id, 1)
 
         # when
         result = leaderboard.add_result(lap_result)
@@ -60,15 +54,9 @@ class LeaderboardTestCase(unittest.TestCase):
 
     def test_should_remove_result(self):
         # given
-        id = len(leaderboard.results)+1
-        lap_result = '59.59.9--1.1--1'
-        expected_result = {
-            'date': '1.1',
-            'time': '59.59.9',
-            'time_in_ds': 35999,
-            'lap': 1,
-            'id': id
-        }
+        id = len(leaderboard.results) + 1
+        lap_result = '59.59.9--1.1.2068--9'
+        expected_result = config_test.get_result_with_id(id, 9)
 
         result_id = leaderboard.add_result(lap_result)
         self.assertEqual(leaderboard.get_result_by_id(result_id), expected_result)
@@ -77,4 +65,4 @@ class LeaderboardTestCase(unittest.TestCase):
         leaderboard.remove_result_by_id(id)
 
         # then
-        self.assertEqual(len(leaderboard.results),id-1)
+        self.assertEqual(len(leaderboard.results), id - 1)
