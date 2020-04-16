b4ebbeff67ec440f2439f97b553b88ffb70023ba
PastorCmentarny <dmspastor@gmail.com>
2020-04-16 11:48:35 +0100

    add sentence from Dr. Mike

--- src/mothership/good_english_sentence.py
+++ src/mothership/good_english_sentence.py
@@ -5,14 +5,15 @@ sentences = [
     'I am extremely sorry for the disruption that Metropolitan line customers are experiencing this morning.'
     'This has been caused by an issue with the signalling system at Baker Street.',
     "Michael Jackson was taking propofol to sleep, which is like doing chemotherapy because you're tired of shaving "
-    "your head. (Robbie Williams)",
+    "your head. (by Robbie Williams)",
     "Brent Cross Shopping Centre is hell on the earth on saturday.",
     "Jeremy Clarskon - '(...) to create a vision of pure, what's the word? May and Hammond 'RUBBISH!",
     "(4B) I shaved off my beard and I went to 'bear & bird' pub to drink a few pints of beer.",
     "I do not have knowledge on that matter.",
     "There've been a few developments.",
     "I am not a teacher, nor do I wish to become one.",  # from ST TNG
-    "It was destined to fail."
+    "It was destined to fail. (by Tim Dillon in PowerfulJRE)",
+    'The medicine is imperfect science. (by Dr. Mikhail "Dr. Mike" Varshavski)'
 ]
 
 
