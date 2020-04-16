64a5bbffb7f653a12e4ecb6b673d8dfb2adfc56b
dominik symonowicz <dmspastor@gmail.com>
2020-04-16 09:07:50 +0100

    add bonus points for on more than first lap

--- src/zeroeighttrack/leaderboard_score.py
+++ src/zeroeighttrack/leaderboard_score.py
@@ -3,20 +3,24 @@ import gobshite_exception
 NORMAL_DISTANCE = 260
 MAX_SCORE = 26888
 
-def calculate_score(time: int, distance: int) -> int:
 
+def calculate_score(time: int, distance: int, lap: int) -> int:
+    score = MAX_SCORE - time
+    if lap > 1:
+        print(score)
+        score = int(score + (score / 200) * (lap - 1))
+        print(score)
     if distance == 0:
-        return MAX_SCORE - time - 450  # move to config?
+        return score - 450  # move to config?
 
     if 261 >= distance >= 259:
-        return MAX_SCORE - time
+        return score
 
     if distance < 259:
-
-        return MAX_SCORE - time - ((NORMAL_DISTANCE - distance) * (NORMAL_DISTANCE - distance)) - int(
+        return score - ((NORMAL_DISTANCE - distance) * (NORMAL_DISTANCE - distance)) - int(
             (NORMAL_DISTANCE - distance) * (time / NORMAL_DISTANCE))
 
     if distance > 261:
-        return MAX_SCORE - time + (distance - NORMAL_DISTANCE)
+        return score + (distance - NORMAL_DISTANCE)
 
     raise gobshite_exception.GobshiteException
--- test/test_leaderboard_score.py
+++ test/test_leaderboard_score.py
@@ -8,16 +8,16 @@ class ScoreCase(unittest.TestCase):
 
     def test_to_deciseconds(self):
         # given
-        scale_params_list = [('perfect distance', 260, 20000), ('too short but in acceptance distance', 259, 20000),
-                             ('too long but in acceptance distance', 261, 20000), ('too short distance', 250, 20869),
-                             ('too long distance', 270, 19990)]
+        scale_params_list = [('perfect distance', 260, 6888), ('too short but in acceptance distance', 259, 6888),
+                             ('too long but in acceptance distance', 261, 6888), ('too short distance', 250, 6019),
+                             ('too long distance', 270, 6898)]
 
         for case, an_input, expected_result in scale_params_list:
             with self.subTest(msg="Checking to deciseconds() for time {} ".format(an_input)):
                 print('testing for {} case with distance {}'.format(case, an_input))
 
                 # when
-                result = leaderboard_score.calculate_score(time, an_input)
+                result = leaderboard_score.calculate_score(time, an_input, 1)
 
                 # then
                 self.assertEqual(expected_result, result)
@@ -26,10 +26,32 @@ class ScoreCase(unittest.TestCase):
         print('testing for forgot to lap it case')
 
         # given
-        expected_result = 20450
+        expected_result = 6438
 
         # when
-        result = leaderboard_score.calculate_score(time, 0)
+        result = leaderboard_score.calculate_score(time, 0, 1)
+
+        # then
+        self.assertEqual(expected_result, result)
+
+    def test_calculate_score_with_bonus_for_extra_lap(self):
+        # given
+        time = 16888
+        expected_result = 10050
+
+        # when
+        result = leaderboard_score.calculate_score(time, 260, 2)
+
+        # then
+        self.assertEqual(expected_result, result)
+
+    def test_calculate_score_with_bonus_for_extra_2_laps(self):
+        # given
+        time = 16888
+        expected_result = 10100
+
+        # when
+        result = leaderboard_score.calculate_score(time, 260, 3)
 
         # then
         self.assertEqual(expected_result, result)
