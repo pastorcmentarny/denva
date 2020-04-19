74a6df967cae3650e0ffbbdbac4a5436b53cc320
PastorCmentarny <dmspastor@gmail.com>
2020-04-19 15:45:02 +0100

    add status

---
+++ src/common/status.py
@@ -0,0 +1,26 @@
+class Status:
+    OK = 2
+    WARN = 1
+    ERROR = 0
+
+    state = OK
+
+    def __init__(self, state: int = 2):
+        self.state = state
+
+    def set_warn(self):
+        if self.state == 2:
+            self.state = 1
+
+    def set_error(self):
+        self.state = 0
+
+    def get_status_as_light_colour(self):
+        if self.state == 2:
+            return 'GREEN'
+        elif self.state == 1:
+            return 'YELLOW'
+        elif self.state == 0:
+            return 'RED'
+        else:
+            print('UNKNOWN STATE {}'.format(self.state))
---
+++ test/test_status.py
@@ -0,0 +1,61 @@
+import unittest
+
+from common import status
+
+
+class StatusCase(unittest.TestCase):
+    def test_status_is_green(self):
+        s = status.Status(2)
+
+        # when
+        result = s.get_status_as_light_colour()
+
+        # then
+        self.assertEqual('GREEN', result)
+
+    def test_status_is_yellow(self):
+        s = status.Status(1)
+
+        # when
+        result = s.get_status_as_light_colour()
+
+        # then
+        self.assertEqual('YELLOW', result)
+
+    def test_status_is_red(self):
+        s = status.Status(0)
+
+        # when
+        result = s.get_status_as_light_colour()
+
+        # then
+        self.assertEqual('RED', result)
+
+    def test_status_set_to_warn(self):
+        s = status.Status(2)
+
+        # when
+        s.set_warn()
+        result = s.get_status_as_light_colour()
+
+        # then
+        self.assertEqual('YELLOW', result)
+
+    def test_status_set_to_error(self):
+        s = status.Status(2)
+
+        # when
+        s.set_error()
+
+        # then
+        self.assertEqual('RED', s.get_status_as_light_colour())
+
+    def test_status_do_not_set_to_warn_if_status_is_set_to_error(self):
+        s = status.Status(2)
+        s.set_error()
+
+        # when
+        s.set_warn()
+
+        # then
+        self.assertEqual('RED', s.get_status_as_light_colour())
