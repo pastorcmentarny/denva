722d34a22e467af4738038514328f737fbcb53cd
PastorCmentarny <dmspastor@gmail.com>
2020-04-20 10:56:39 +0100

    make animation smooth (WIP)

--- src/app_delight.py
+++ src/app_delight.py
@@ -171,7 +171,7 @@ def to_x(i: int) -> int:
 
 # TODO prorotype
 def device_status():
-    unicornhathd.rotation(90)
+    unicornhathd.rotation(270)
 
     delight_display.reset_screen()
     cfg = config_service.load_cfg()
@@ -324,13 +324,44 @@ def device_status():
     unicornhathd.set_pixel(to_x(15), 3, purple_r, purple_g, purple_b)
     set_status_for_device(13, 13, color_red, color_green, color_blue)
 
+    b1 = [0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19]
+    b2 = [0.20, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29]
+    b3 = [0.30, 0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39]
+    b4 = [0.40, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49]
+    b5 = [0.50, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59]
+    b6 = [0.60, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69]
+    brightnesses = [0.3]
+    brightnesses.extend(b3)
+    brightnesses.extend(b4)
+    brightnesses.extend(b5)
+    brightnesses.extend(b6)
+    b1.reverse()
+    b2.reverse()
+    b3.reverse()
+    b4.reverse()
+    b5.reverse()
+    b6.reverse()
+    brightnesses.extend(b6)
+    brightnesses.extend(b5)
+    brightnesses.extend(b4)
+    brightnesses.extend(b3)
+    brightnesses.extend(b2)
+    brightnesses.extend(b1)
+    b1.reverse()
+    b2.reverse()
+    b3.reverse()
+    b4.reverse()
+    b5.reverse()
+    b6.reverse()
+    brightnesses.extend(b1)
+    brightnesses.extend(b2)
 
-    brightnesses = [0.3, 0.4, 0.5, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.2, 0.3]
     for x in range(0, 3):
         for b in brightnesses:
             unicornhathd.brightness(b)
-            time.sleep(1)
+            time.sleep(0.1)
             unicornhathd.show()
+
     unicornhathd.rotation(180)
 
 
