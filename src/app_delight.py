d224512fc7f8b1d8b853f3c89e913c447140eeba
PastorCmentarny <dmspastor@gmail.com>
2020-04-21 19:39:49 +0100

    fix typo

--- src/app_delight.py
+++ src/app_delight.py
@@ -301,25 +301,25 @@ def device_status():
 
     # 4.
     state = status.Status()
-    system_data = delight_service.get_system_info()
+    delight_data = delight_service.get_system_info()
 
-    if float(utils.get_float_number_from_text(server_data['CPU Temp'])) > cfg['sensor']['cpu_temp_error']:
+    if float(utils.get_float_number_from_text(delight_data['CPU Temp'])) > cfg['sensor']['cpu_temp_error']:
         logger.warning('status: RED due to very high cpu temp on Delight')
         state.set_error()
-    elif float(utils.get_float_number_from_text(server_data['CPU Temp'])) > cfg['sensor']['cpu_temp_warn']:
+    elif float(utils.get_float_number_from_text(delight_data['CPU Temp'])) > cfg['sensor']['cpu_temp_warn']:
         logger.warning('status: ORANGE due to high cpu temp on Delight')
         state.set_warn()
-    if utils.get_int_number_from_text(system_data['Memory Available']) < 128:
+    if utils.get_int_number_from_text(delight_data['Memory Available']) < 128:
         logger.warning('status: RED due to very low memory available on Delight')
         state.set_error()
-    elif utils.get_int_number_from_text(system_data['Memory Available']) < 256:
+    elif utils.get_int_number_from_text(delight_data['Memory Available']) < 256:
         logger.warning('status: ORANGE due to low memory available on Delight')
         state.set_warn()
 
-    if utils.get_int_number_from_text(system_data['Free Space']) < 128:
+    if utils.get_int_number_from_text(delight_data['Free Space']) < 128:
         logger.warning('status: RED due to very low free space on Delight')
         state.set_error()
-    elif utils.get_int_number_from_text(system_data['Free Space']) < 512:
+    elif utils.get_int_number_from_text(delight_data['Free Space']) < 512:
         logger.warning('status: ORANGE due to low free space on Delight')
         state.set_warn()
 
