292ca28b0c50d21f3d597e21273a65a080cd382f
PastorCmentarny <dmspastor@gmail.com>
2020-05-05 14:05:24 +0100

    update update script to it preserve zip until is downloaded from git

--- scripts/update.sh
+++ scripts/update.sh
@@ -9,10 +9,11 @@ then
 else
     echo "re-run project using branch $1"
     cd /home/pi
-    rm "$1.zip"
+    mv "$1.zip" "$1-backup.zip"
     rm -rf "denva-$1"
     wget "https://github.com/pastorcmentarny/denva/archive/$1.zip"
     unzip "$1.zip"
+    rm "$1-backup.zip"
     cd "denva-$1/"
     #pip install -r "requirements.txt "
     cd "src/"
