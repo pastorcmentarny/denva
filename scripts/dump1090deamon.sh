665ffe2d8cb6b6280689d33ac7b85c6c0bc31b20
PastorCmentarny <dmspastor@gmail.com>
2020-05-09 14:36:44 +0100

    update script to run dump1090 as service

--- scripts/dump1090deamon.sh
+++ scripts/dump1090deamon.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-#!/bin/bash
 ### BEGIN INIT INFO
 #
 # Provides:		dump1090
@@ -15,60 +14,60 @@
 ## Fill in name of program here.
 PROG="dump1090"
 PROG_PATH="/home/pi/dump1090"
-PROG_ARGS="--interactive --net --metrics --net-beast --net-ro-port 31001"
+PROG_ARGS="--net --net-http-port 16601  --metric --quiet"
 PIDFILE="/var/run/dump1090.pid"
 
 start() {
-      if [ -e $PIDFILE ]; then
-          ## Program is running, exit with error.
-          echo "Error! $PROG is currently running!" 1>&2
-          exit 1
-      else
-          ## Change from /dev/null to something like /var/log/$PROG if you want to save output.
-          cd $PROG_PATH
-          ./$PROG $PROG_ARGS 2>&1 >/dev/null &
-          echo "$PROG started"
-          touch $PIDFILE
-      fi
+  if [ -e $PIDFILE ]; then
+    ## Program is running, exit with error.
+    echo "Error! $PROG is currently running!" 1>&2
+    exit 1
+  else
+    ## Change from /dev/null to something like /var/log/$PROG if you want to save output.
+    cd $PROG_PATH
+    ./$PROG $PROG_ARGS 2>&1 >/var/log/$PROG &
+    echo "$PROG started"
+    touch $PIDFILE
+  fi
 }
 
 stop() {
-      if [ -e $PIDFILE ]; then
-          ## Program is running, so stop it
-         echo "$PROG is running"
-         killall $PROG
-         rm -f $PIDFILE
-         echo "$PROG stopped"
-      else
-          ## Program is not running, exit with error.
-          echo "Error! $PROG not started!" 1>&2
-          exit 1
-      fi
+  if [ -e $PIDFILE ]; then
+    ## Program is running, so stop it
+    echo "$PROG is running"
+    killall $PROG
+    rm -f $PIDFILE
+    echo "$PROG stopped"
+  else
+    ## Program is not running, exit with error.
+    echo "Error! $PROG not started!" 1>&2
+    exit 1
+  fi
 }
 
 ## Check to see if we are running as root first.
 if [ "$(id -u)" != "0" ]; then
-      echo "This script must be run as root" 1>&2
-      exit 1
+  echo "This script must be run as root" 1>&2
+  exit 1
 fi
 
 case "$1" in
-      start)
-          start
-          exit 0
-      ;;
-      stop)
-          stop
-          exit 0
-      ;;
-      reload|restart|force-reload)
-          stop
-          start
-          exit 0
-      ;;
-      **)
-          echo "Usage: $0 {start|stop|reload}" 1>&2
-          exit 1
-      ;;
+start)
+  start
+  exit 0
+  ;;
+stop)
+  stop
+  exit 0
+  ;;
+reload | restart | force-reload)
+  stop
+  start
+  exit 0
+  ;;
+**)
+  echo "Usage: $0 {start|stop|reload}" 1>&2
+  exit 1
+  ;;
 esac
 #
