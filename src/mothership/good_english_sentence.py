4bbb476aaa77f7cfd0406aa08df2b98bc27f7efd
PastorCmentarny <dmspastor@gmail.com>
2020-05-28 23:31:41 +0100

    add a few more quotes for good_english_sentence

--- src/mothership/good_english_sentence.py
+++ src/mothership/good_english_sentence.py
@@ -24,7 +24,10 @@ sentences = [
     "The mechanics that allow us to think and reason and contemplate didn't exist millions of years ago. The first fish to crawl onto land aeons ago wasn't racked with self-doubt, thinking, 'Why am I doing this? I can't breathe up here, and I don't even have any legs, whatever they are.  ('The Idiot brain' by Dean Burnett).",
     "Sleep involves doing nothing literally, lying down and not being conscious. How complicated could it possibly be?   ('The Idiot brain' by Dean Burnett).",
     "If you can't retrieve a memory, it's as good as not being there at all. It's like when you can't find your gloves; you've still got gloves, they still exist, but you've got cold hands anyway. ('The Idiot brain' by Dean Burnett).",
-    "Brain never shuts down completely, partly because it has several ... the sleep state, but mostly because if it did shut down completely we'd be dead. ('The Idiot brain' by Dean Burnett)"
+    "Brain never shuts down completely, partly because it has several ... the sleep state, but mostly because if it did shut down completely we'd be dead. ('The Idiot brain' by Dean Burnett)",
+    "It's bit like using bathroom scales to weigh elephants; they can be useful for a standard range of weights, but at this level they'll give no useful data, just a load of broken plastic and springs ('The Idiot brain' by Dean Burnett)",
+    "Modern politicians are media-trained so they can speak confidently and smoothly on any subject for prolonged periods without saying anything of value ('The Idiot brain' by Dean Burnett)",
+    "You know for as long I can remember I had memories. (by Colin Mochrie)"
 ]
 
 
