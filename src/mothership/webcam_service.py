c02c6f3883a1baa26e70930e11b9ff4662f7bbee
PastorCmentarny <dmspastor@gmail.com>
2020-06-02 13:40:46 +0100

    code cleanup for capture_picture

--- src/mothership/webcam_service.py
+++ src/mothership/webcam_service.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from datetime import datetime
+from pathlib import Path
 from timeit import default_timer as timer
 
 import cv2
@@ -12,19 +13,30 @@ webcam = cv2.VideoCapture(0)
 logger = logging.getLogger('app')
 step = 0.1
 
+'''
+if you see this, check is privacy setting for camera is set
+"cv2.error: OpenCV(4.1.2) C:\\projects\\opencv-python\\opencv\\modules\\imgcodecs\\src\\loadsave.cpp:715: 
+error: (-215:Assertion failed) !_img.empty() in function 'cv::imwrite'\n",
+#TODO add detection permission for camera is ON 
+'''
+
 
 def capture_picture() -> str:
     logger.info('capturing a picture')
     try:
         start_time = timer()
-        date_path = datetime.now().strftime("%Y\\%m\\%d")
-        path = "E:\\cctv\\{}".format(date_path)
-        if not os.path.isdir(path):
-            logger.info('creating folder for {}'.format(path))
-            os.makedirs(path)
-        date = dom_utils.get_timestamp_file()
-        photo_path = '{}\\{}.jpg'.format(path, date)
-        logger.info(photo_path)
+        date_as_folders = dom_utils.get_date_as_folders_linux()
+        path = Path("{}/{}".format("e:/cctv/", date_as_folders))
+
+        if not path.exists():
+            print('creating')
+            Path(path).mkdir(parents=True, exist_ok=True)
+
+        file = dom_utils.get_date_with_time_as_filename("cctv", "jpg", datetime.now())
+
+        photo_path = Path('{}/{}'.format(path, file))
+
+        logger.info('using path {}'.format(photo_path))
         check, frame = webcam.read()
         cv2.imwrite(photo_path, frame)
         logger.info('saving image to file')
@@ -36,7 +48,6 @@ def capture_picture() -> str:
         logger.info('it took {} ms to generate picture.'.format(total_time))
         return photo_path
     except Exception as e:
-        logger.warning('Something went badly wrong\n{}'.format(e), exc_info=True)
+        logger.warning('Unable to capture picture due to {}'.format(e), exc_info=True)
         # TODO email_sender_service.send_error_log_email("camera", "Unable to capture picture due to {}".format(e))
-    logger.warning('No path returned due to previous error.')
     return ""
