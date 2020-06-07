06cb95dd70aec4ee518ef88ae5d9a32f0022aba6
PastorCmentarny <dmspastor@gmail.com>
2020-06-07 19:53:46 +0100

    save results when run project_line_counter

--- scripts/project_line_counter.py
+++ scripts/project_line_counter.py
@@ -1,16 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+* Author Dominik Symonowicz
+* WWW:	https://dominiksymonowicz.com/welcome
+* IT BLOG:	https://dominiksymonowicz.blogspot.co.uk
+* Github:	https://github.com/pastorcmentarny
+* Google Play:	https://play.google.com/store/apps/developer?id=Dominik+Symonowicz
+* LinkedIn: https://www.linkedin.com/in/dominik-symonowicz
+"""
+
 import os.path
 import pathlib
+from datetime import datetime
+
+from itertools import chain
 
 line_count = 0
 file_count = 0
+result_file = str(pathlib.PurePath("D:/GitHub/denva/scripts/","project_line_counter.py"))
 
-for path, _, files in os.walk("D:\\GitHub\\denva\\src\\"):
+paths = ("D:\\GitHub\\denva\\src\\", "D:\\GitHub\\denva\\scripts\\")
+for path, _, files in chain.from_iterable(os.walk(path) for path in paths):
     for name in files:
         file_path = pathlib.PurePath(path, name)
-        if str(file_path).endswith(".py"):
+        if str(file_path).endswith(".py") or str(file_path).endswith(".sh"):
             with open(str(file_path), encoding="utf-8") as f:
                 file_count += 1
                 for line in f:
                     line_count += 1
 
-print("There is a {} files in the project and total count of lines in the project is: {}".format(file_count,line_count))
+result = ("# {}.There are {} files in the project and total count of lines is: {}".format(datetime.now(), file_count,
+                                                                                        line_count))
+with open(result_file, 'a+', newline='') as local_file:
+    local_file.write(result)
+    local_file.write('\n')
+
+# Results
+# 2020-06-07 19:52:09.065886.There are 89 files in the project and total count of lines is: 8694
+
