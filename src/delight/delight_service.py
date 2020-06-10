6b59f8a95865d0bb52d073e18ec67fbfb98626d9
PastorCmentarny <dmspastor@gmail.com>
2020-06-10 15:34:06 +0100

    add highest and fastest flight to stats

--- src/delight/delight_service.py
+++ src/delight/delight_service.py
@@ -61,7 +61,8 @@ def get_flights_for_today() -> dict:
     data = aircraft_storage.load_processed_data()
     return {
         'detected': aircraft_stats.count_aircraft_found(data),
-        'flights': aircraft_stats.get_flights_found(data)
+        'flights': aircraft_stats.get_flights_found(data),
+        'stats': aircraft_stats.get_stats(data)
     }
 
 
@@ -69,7 +70,8 @@ def get_flights_for_yesterday():
     data = aircraft_storage.load_processed_for_yesterday()
     return {
         'detected': aircraft_stats.count_aircraft_found(data),
-        'flights': aircraft_stats.get_flights_found(data)
+        'flights': aircraft_stats.get_flights_found(data),
+        'stats': aircraft_stats.get_stats(data)
     }
 
 
