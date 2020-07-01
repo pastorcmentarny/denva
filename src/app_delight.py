92b090a276a294fe64bb34e8c54cc47613f1db03
PastorCmentarny <dmspastor@gmail.com>
2020-07-01 15:33:38 +0100

    extract and refactor perform_blink_animation

--- src/app_delight.py
+++ src/app_delight.py
@@ -36,8 +36,8 @@ from systemhc import system_health_prototype
 logger = logging.getLogger('app')
 
 unicornhathd.rotation(180)
-DEFAULT_BRIGHTNESS_LEVEL = 0.3
-unicornhathd.brightness(DEFAULT_BRIGHTNESS_LEVEL)
+
+unicornhathd.brightness(config_service.get_default_brightness_for_delight_display())
 
 clock = 0
 cycle = 0
@@ -128,9 +128,9 @@ def device_status():
         time.sleep(15)
     else:
         if blink:
-            perform_state_animation()
+            ui_utils.perform_blink_animation(unicornhathd)
         else:
-            unicornhathd.brightness(DEFAULT_BRIGHTNESS_LEVEL)
+            unicornhathd.brightness(config_service.get_default_brightness_for_delight_display())
             unicornhathd.show()
             time.sleep(30)
 
@@ -300,43 +300,6 @@ def set_denva_status(cfg):
     return blink
 
 
-def perform_state_animation():
-    b1 = [0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19]
-    b2 = [0.20, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29]
-    b3 = [0.30, 0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39]
-    b4 = [0.40, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49]
-    b5 = [0.50, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59]
-    b6 = [0.60, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69]
-    brightnesses = [DEFAULT_BRIGHTNESS_LEVEL]
-    brightnesses.extend(b3)
-    brightnesses.extend(b4)
-    brightnesses.extend(b5)
-    brightnesses.extend(b6)
-    b1.reverse()
-    b2.reverse()
-    b3.reverse()
-    b4.reverse()
-    b5.reverse()
-    b6.reverse()
-    brightnesses.extend(b6)
-    brightnesses.extend(b5)
-    brightnesses.extend(b4)
-    brightnesses.extend(b3)
-    brightnesses.extend(b2)
-    brightnesses.extend(b1)
-    b1.reverse()
-    b2.reverse()
-    b3.reverse()
-    b4.reverse()
-    b5.reverse()
-    b6.reverse()
-    brightnesses.extend(b1)
-    brightnesses.extend(b2)
-    for x in range(0, 15):
-        for b in brightnesses:
-            unicornhathd.brightness(b)
-            unicornhathd.show()
-            time.sleep(0.025)
 
 
 def is_night_mode() -> bool:
