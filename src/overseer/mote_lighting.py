8c125b02627c82ae4c766b5d7e51f5d864fe8b50
Dominik Symonowicz <dmspastor@gmail.com>
2021-03-27 23:04:32 +0000

    make some adjustment for borg mode

--- src/overseer/mote_lighting.py
+++ src/overseer/mote_lighting.py
@@ -226,16 +226,13 @@ def yellow_alert():
 
 def borg():
     logger.info('We are the Borg. Resistance is futile')
-    blink_speed = 1
+    blink_speed = 0.5
     mote.set_brightness(0.1)
-    for times in range(100):
+    for times in range(10):
         for led_index in range(0, 16):
             for led_line in range(1, 5):
                 mote.set_pixel(led_line, led_index, 0, random.randint(0, 255), 0)
         mote.set_pixel(random.randint(1, 4), random.randint(0, 15), 0, 255, 0, random.randint(1, 10) / 10)
-        mote.show()
-        time.sleep(blink_speed)
-        set_color_for(BLACK)
         mote.show()
         time.sleep(blink_speed)
 
