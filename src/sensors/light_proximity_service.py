878b1a669dfd30ea90ab50b608d9b04ba7a3e115
Dominik Symonowicz <dmspastor@gmail.com>
2021-05-13 09:16:49 +0100

    add rgb metrics support for denva

--- src/sensors/light_proximity_service.py
+++ src/sensors/light_proximity_service.py
@@ -9,18 +9,30 @@
 * Google Play:	https://play.google.com/store/apps/developer?id=Dominik+Symonowicz
 * LinkedIn: https://www.linkedin.com/in/dominik-symonowicz
 """
+from gateways import local_data_gateway
 
-try:
-    # Transitional fix for breaking change in LTR559
-    from ltr559 import LTR559
 
-    ltr559 = LTR559()
-except ImportError:
-    import ltr559
+def setup():
+    try:
+        # Transitional fix for breaking change in LTR559
+        from ltr559 import LTR559
+        ltr559 = LTR559()
+    except ImportError:
+        import ltr559
+
+    return ltr559
+
+
+ltr559 = setup()
 
 
 def get_illuminance():
-    return ltr559.get_lux()
+    try:
+        lux = ltr559.get_lux()
+        local_data_gateway.post_metrics_update('pollution', 'OK')
+        return lux
+    except Exception as exception:
+        setup()
 
 
 def get_proximity():
