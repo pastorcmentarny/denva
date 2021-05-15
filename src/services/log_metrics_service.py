2d3e27b93f786140884c87832e93db7756560385
Dominik Symonowicz <dmspastor@gmail.com>
2021-05-15 18:52:18 +0100

    add prototype for log metrics service

---
+++ src/services/log_metrics_service.py
@@ -0,0 +1,70 @@
+import copy
+
+DEBUG = 'DEBUG'
+INFO = 'INFO'
+WARNING = 'WARNING'
+CRITICAL = 'CRITICAL'
+ERROR = 'ERROR'
+ENCODING = 'utf-8'
+TOTAL_COUNT = 'total_count'
+
+
+def load_logs(path):
+    with open(path, 'r', encoding=ENCODING) as logs_file:
+        return logs_file.read().splitlines()
+
+
+log_metrics = {
+    CRITICAL: 0,
+    ERROR: 0,
+    WARNING: 0,
+    INFO: 0,
+    DEBUG: 0,
+    TOTAL_COUNT: 0
+}
+
+
+def get_total_count_of_log() -> int:
+    return log_metrics[CRITICAL] + log_metrics[ERROR] + log_metrics[WARNING] + log_metrics[INFO] + log_metrics[DEBUG]
+
+
+def get_stats_for(log_type) -> str:
+    return f'{log_type.lower()} count: {log_metrics[log_type]} which is {"%.2f" % (log_metrics[log_type] / get_total_count_of_log() * 100)}% of all logs.'
+
+
+def shows_stats_for_log_metrics():
+    all_log_types = [CRITICAL, ERROR, WARNING, INFO, DEBUG]
+    for log_type in all_log_types:
+        print(get_stats_for(log_type))
+
+
+def generate_log_stats(path: str):
+    log_data = load_logs(path)
+    for row in log_data:
+
+        if ' - CRITICAL - ' in row:
+            log_metrics[CRITICAL] = log_metrics[CRITICAL] + 1
+        elif ' - ERROR - ' in row:
+            log_metrics[ERROR] = log_metrics[ERROR] + 1
+        elif ' - WARNING - ' in row:
+            log_metrics[WARNING] = log_metrics[WARNING] + 1
+        elif ' - INFO - ' in row:
+            log_metrics[INFO] = log_metrics[INFO] + 1
+        elif ' - DEBUG - ' in row:
+            log_metrics[DEBUG] = log_metrics[DEBUG] + 1
+
+    log_metrics[TOTAL_COUNT] = get_total_count_of_log()
+    print(shows_stats_for_log_metrics())
+
+
+def get_log_metrics() -> dict:
+    return copy.deepcopy(log_metrics)
+
+
+def clear():
+    log_metrics[CRITICAL] = 0
+    log_metrics[ERROR] = 0
+    log_metrics[WARNING] = 0
+    log_metrics[INFO] = 0
+    log_metrics[DEBUG] = 0
+    log_metrics[TOTAL_COUNT] = 0
---
+++ test/test_log_metrics_service.py
@@ -0,0 +1,68 @@
+import unittest
+
+from services import log_metrics_service
+
+TEST_PATH = 'B:\\GitHub\\denva\\resources_data\\test-log.txt'
+
+
+class LogMetricsTestCase(unittest.TestCase):
+
+    def test_generate_log_stats(self):
+        # given
+        log_metrics_service.clear()
+
+        expected_result = {'CRITICAL': 17,
+                           'DEBUG': 0,
+                           'ERROR': 12,
+                           'INFO': 83633,
+                           'WARNING': 1,
+                           'total_count': 83663}
+        # when
+        log_metrics_service.generate_log_stats(TEST_PATH)
+
+        # then
+        self.assertEqual(log_metrics_service.get_log_metrics(), expected_result)
+
+    def test_get_stats_for(self):
+        # given
+        log_metrics_service.clear()
+
+        log_metrics_service.generate_log_stats(TEST_PATH)
+        expected_result = 'error count: 12 which is 0.01% of all logs.'
+        # when
+        result = log_metrics_service.get_stats_for('ERROR')
+
+        # then
+        self.assertEqual(result, expected_result)
+
+    def test_clear(self):
+        # given
+        log_metrics_service.clear()
+        log_metrics_service.generate_log_stats(TEST_PATH)
+        verify_data = {'CRITICAL': 17,
+                       'DEBUG': 0,
+                       'ERROR': 12,
+                       'INFO': 83633,
+                       'WARNING': 1,
+                       'total_count': 83663}
+        expected_result = {'CRITICAL': 0,
+                           'DEBUG': 0,
+                           'ERROR': 0,
+                           'INFO': 0,
+                           'WARNING': 0,
+                           'total_count': 0}
+        # verify
+        log_metrics_data = log_metrics_service.get_log_metrics()
+        self.assertEqual(log_metrics_data, verify_data)
+
+        # when
+        log_metrics_service.clear()
+        result = log_metrics_service.get_log_metrics()
+
+        # then
+        self.assertNotEquals(result, verify_data)
+        self.assertEqual(result,expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
