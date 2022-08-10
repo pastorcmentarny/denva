72c81eb60e475ea3772c62649be039edbcb7c32c
Dominik Symonowicz <dmspastor@gmail.com>
2022-08-10 14:44:40 +0100

    add retry to save file

--- src/server/server_storage_service.py
+++ src/server/server_storage_service.py
@@ -1,7 +1,10 @@
 import json
+import logging
+from retrying import retry
 
 MODE = 'w'
 ENCODING = 'utf-8'
+logger = logging.getLogger('www')
 
 
 def save_denva_measurement(data):
@@ -16,6 +19,16 @@ def save_trases_measurement(data):
     __save(data, '/home/pi/data/trases_data.json')
 
 
+def __retry_on_exception(exception):
+    return isinstance(exception, Exception)
+
+
+@retry(retry_on_exception=__retry_on_exception, wait_exponential_multiplier=50, wait_exponential_max=1000,
+       stop_max_attempt_number=5)
 def __save(data, path):
-    with open(path, MODE, encoding=ENCODING) as trases_measurement_file:
-        json.dump(data, trases_measurement_file, ensure_ascii=False, indent=4)
+    try:
+        with open(path, MODE, encoding=ENCODING) as trases_measurement_file:
+            json.dump(data, trases_measurement_file, ensure_ascii=False, indent=4)
+        return ""
+    except Exception as exception:
+        logger.error(f"Unable to save due to : {exception}", exc_info=True)
