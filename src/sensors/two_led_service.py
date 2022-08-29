ebdae2c2af35c57ccd0d8d1f2412e30def11b1a8
Dominik Symonowicz <dmspastor@gmail.com>
2022-08-29 13:32:17 +0100

    clean up code in two led lights on  bh1745 (two_led_service)

--- src/sensors/two_led_service.py
+++ src/sensors/two_led_service.py
@@ -16,9 +16,11 @@ from bh1745 import BH1745
 import config
 import logging
 
-
-# Set up light sensor
 from gateways import local_data_gateway
+
+LED_OFF = 0
+
+LED_ON = 1
 
 logger = logging.getLogger('app')
 bh1745 = BH1745()
@@ -29,27 +31,27 @@ bh1745.set_leds(1)
 def warn_if_dom_shakes_his_legs(motion):
     if motion > config.get_shaking_level():
         for i in range(5):
-            bh1745.set_leds(1)
+            bh1745.set_leds(LED_ON)
             time.sleep(0.25)
-            bh1745.set_leds(0)
+            bh1745.set_leds(LED_OFF)
             time.sleep(0.15)
 
 
 def led_startup_show():
     for i in range(5):
-        bh1745.set_leds(1)
+        bh1745.set_leds(LED_ON)
         time.sleep(0.2)
-        bh1745.set_leds(0)
+        bh1745.set_leds(LED_OFF)
         time.sleep(0.05)
-    bh1745.set_leds(0)
+    bh1745.set_leds(LED_OFF)
 
 
 def on():
-    bh1745.set_leds(1)
+    bh1745.set_leds(LED_ON)
 
 
 def off():
-    bh1745.set_leds(0)
+    bh1745.set_leds(LED_OFF)
 
 
 def get_measurement():
@@ -67,6 +69,16 @@ def get_measurement():
 def switch_led(led_status):
     bh1745.set_leds(led_status)
     if led_status == 1:
-        return 0
+        return LED_OFF
     else:
-        return 1
+        return LED_ON
+
+
+def error_blink():
+    blink_length = [0.1, 0.16, 0.25, 0.36, 0.49, 0.64]
+    for i in range(5):
+        bh1745.set_leds(LED_ON)
+        time.sleep(blink_length[i])
+        bh1745.set_leds(LED_OFF)
+        time.sleep(blink_length[i])
+    bh1745.set_leds(LED_OFF)
