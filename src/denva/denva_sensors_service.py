e095f6974833e2525814618f2925d6d0649605ea
Dominik Symonowicz <dmspastor@gmail.com>
2022-09-01 16:30:06 +0100

    remove unused old measurement

--- src/denva/denva_sensors_service.py
+++ src/denva/denva_sensors_service.py
@@ -2,8 +2,8 @@ import re
 from datetime import datetime
 
 import config as config
-from common import data_files, commands, loggy
 import dom_utils
+from common import data_files, commands, loggy
 
 
 def load_data_for_today() -> list:
@@ -18,130 +18,30 @@ def get_sensor_log_file_at_server() -> str:
     return config.PI_SENSORS_DATA_PATH + 'denva/' + dom_utils.get_date_as_filename('sensor-log', 'csv', datetime.now())
 
 
-def get_last_old_measurement():
-    entry = commands.get_last_line_from_log(get_sensor_log_file())
-    data = entry.split(',')
-    return get_old_data_row(data)
-
-
 def get_last_new_measurement():
     entry = commands.get_last_line_from_log(get_sensor_log_file())
     data = entry.split(',')
     return get_new_data_row(data)
 
 
-def get_old_data_row(row) -> dict:
-    return {
-        'timestamp': row[0],
-        'temp': row[1],
-        'pressure': row[2],
-        'humidity': '{:0.2f}'.format(float(row[3])),
-        'gas_resistance': '{:0.2f}'.format(float(row[4])),
-        'colour': row[5],
-        'aqi': row[6],
-        'uva_index': '{:0.2f}'.format(float(row[7])),
-        'uvb_index': '{:0.2f}'.format(float(row[8])),
-        'motion': '{:0.2f}'.format(float(row[9])),
-        'ax': '{:0.2f}'.format(float(row[10])),
-        'ay': '{:0.2f}'.format(float(row[11])),
-        'az': '{:0.2f}'.format(float(row[12])),
-        'gx': '{:0.2f}'.format(float(row[13])),
-        'gy': '{:0.2f}'.format(float(row[14])),
-        'gz': '{:0.2f}'.format(float(row[15])),
-        'mx': '{:0.2f}'.format(float(row[16])),
-        'my': '{:0.2f}'.format(float(row[17])),
-        'mz': '{:0.2f}'.format(float(row[18])),
-        'cpu_temp': row[20],
-        'eco2': row[21],
-        'tvoc': row[22],
-    }
-
-
-def get_old_warnings(data) -> dict:
-    warnings = {}
-    if type(data['temp']) is not float:
-        data['temp'] = float(data['temp'])
-    if data['temp'] < 16:
-        warnings['temp'] = 'Temperature is too low [tle]. Current temperature is: {}'.format(str(data['temp']))
-    elif data['temp'] < 18:
-        warnings['temp'] = 'Temperature is low [tlw]. Current temperature is: {}'.format(str(data['temp']))
-    elif data['temp'] > 25:
-        warnings['temp'] = 'Temperature is high [thw]. Current temperature is: {}'.format(str(data['temp']))
-    elif data['temp'] > 30:
-        warnings['temp'] = 'Temperature is too high  [the]. Current temperature is: {}'.format(str(data['temp']))
-
-    data['humidity'] = float(data['humidity'])
-
-    if data['humidity'] < 30:
-        warnings['humidity'] = 'Humidity is too low [hle]. Current humidity is: {}'.format(str(data['humidity']))
-    elif data['humidity'] < 40:
-        warnings['humidity'] = 'Humidity is low [hlw]. Current humidity is: {}'.format(str(data['humidity']))
-    elif data['humidity'] > 60:
-        warnings['humidity'] = 'Humidity is high [hhw]. Current humidity is: {}'.format(str(data['humidity']))
-    elif data['humidity'] > 70:
-        warnings['humidity'] = 'Humidity is too high [hhe]. Current humidity is: {}'.format(str(data['humidity']))
-
-    data['uva_index'] = float(data['uva_index'])
-
-    if data['uva_index'] > 6:
-        warnings['uva_index'] = 'UV A is too high [uvae]. Current UV A is: {}'.format(str(data['uva_index']))
-
-    data['uvb_index'] = float(data['uvb_index'])
-
-    if data['uvb_index'] > 6:
-        warnings['uvb_index'] = 'UV B is too high [uvbe]. Current UV B is: {}'.format(str(data['uvb_index']))
-
-    data['cpu_temp'] = float(re.sub('[^0-9.]', '', data['cpu_temp']))
-
-    sensor_config_data = config.load_cfg()
-
-    if data['cpu_temp'] > sensor_config_data['system']['cpu_temp_fatal']:
-        warnings['cpu_temp'] = 'CPU temperature is too high [cthf]. Current temperature is: {}'.format(
-            str(data['cpu_temp']))
-    elif data['cpu_temp'] > sensor_config_data['system']['cpu_temp_error']:
-        warnings['cpu_temp'] = 'CPU temperature is very high [cthe]. Current temperature is: {}'.format(
-            str(data['cpu_temp']))
-    elif data['cpu_temp'] > sensor_config_data['system']['cpu_temp_warn']:
-        warnings['cpu_temp'] = 'CPU temperature is high [cthw]. Current temperature is: {}'.format(
-            str(data['cpu_temp']))
-
-    data['motion'] = float(data['motion'])
-
-    if data['motion'] > 1000:
-        warnings['motion'] = 'Dom is shaking his legs [slw]. Value: {}'.format(str(data['motion']))
-
-    if int(commands.get_space_available()) < 500:
-        warnings['free_space'] = 'Low Free Space: {}'.format(commands.get_space_available() + 'MB')
-
-    if int(data['eco2']) > 1000:
-        warnings['eco2'] = 'High CO2 level: {}'.format(data['eco2'])
-
-    if int(data['tvoc']) > 5000:
-        warnings['tvoc'] = 'Air Quality BAD: {}'.format(data['tvoc'])
-    elif int(data['tvoc']) > 1500:
-        warnings['tvoc'] = 'Air Quality POOR: {}'.format(data['tvoc'])
-    loggy.log_error_count(warnings)
-    return warnings
-
-
 def get_new_data_row(row) -> dict:
     return {
-        'timestamp': row[0],
-        'temp': row[2],
-        'pressure': row[3],
-        'humidity': '{:0.2f}'.format(float(row[4])),
-        'gas_resistance': '{:0.2f}'.format(float(row[5])),
-        'colour': row[6],
-        'r': row[7],
-        'g': row[8],
-        'b': row[9],
-        'co2': row[10],
-        'co2_temperature': row[11],
-        'relative_humidity': '{:0.2f}'.format(float(row[12])),
-        'cpu_temp': row[13],
-        'eco2': row[14],
-        'tvoc': row[15],
-        'gps_num_sats': row[22],
+        config.FIELD_TIMESTAMP: row[0],
+        config.FIELD_TEMPERATURE: row[2],
+        config.FIELD_PRESSURE: row[3],
+        config.FIELD_HUMIDITY: '{:0.2f}'.format(float(row[4])),
+        config.FIELD_GAS_RESISTANCE: '{:0.2f}'.format(float(row[5])),
+        config.FIELD_COLOUR: row[6],
+        config.FIELD_RED: row[7],
+        config.FIELD_GREEN: row[8],
+        config.FIELD_BLUE: row[9],
+        config.FIELD_CO2: row[10],
+        config.FIELD_CO2_TEMPERATURE: row[11],
+        config.FIELD_RELATIVE_HUMIDITY: '{:0.2f}'.format(float(row[12])),
+        config.FIELD_CPU_TEMP: row[13],
+        config.FIELD_ECO2: row[14],
+        config.FIELD_TVOC: row[15],
+        config.FIELD_GPS_NUM_SATS: row[22],
     }
 
 
