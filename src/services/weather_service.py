0b694f378b97d73403bcd0df3cb6f9b649200293
Dominik Symonowicz <dmspastor@gmail.com>
2022-09-14 09:07:30 +0100

    clean up weather service

--- src/services/weather_service.py
+++ src/services/weather_service.py
@@ -17,6 +17,8 @@ import config
 from common import app_timer, data_files
 from gateways import web_data_gateway
 
+NO_WEATHER_DATA_MESSAGE = 'Weather data N/A'
+
 logger = logging.getLogger('app')
 
 
@@ -30,46 +32,45 @@ def save_weather_to_file(weather_data: list, weather_file: str):
     data_files.save_list_to_file(weather_data, weather_file)
 
 
-def is_weather_data_expired(date: str) -> bool:
-    last_check = date.split('-')
-    return app_timer.is_time_to_run_every_6_hours(datetime(int(last_check[0]), int(last_check[1]), int(last_check[2]),
-                                                           int(last_check[3]), int(last_check[4]), int(last_check[5])))
-
-
-# FIXME
 def get_weather() -> list:
     logger.info('Getting weather')
     weather_file = config.PI_DATA_PATH + 'weather.txt'
     if not os.path.exists(weather_file):
         logger.info('File not exists. Getting weather from the web')
-        weather_data = cleanup_weather_data(web_data_gateway.get_weather())
+        weather_data = __cleanup_weather_data(web_data_gateway.get_weather())
     else:
         logger.info('loading data from the file')
         weather_data = data_files.load_weather(weather_file)
-        if is_weather_data_expired(weather_data[len(weather_data) - 1]):
+        if __is_weather_data_expired(weather_data[len(weather_data) - 1]):
             logger.info('Weather in the file is out of date, Getting weather from the web')
-            weather_data = cleanup_weather_data(web_data_gateway.get_weather())
+            weather_data = __cleanup_weather_data(web_data_gateway.get_weather())
         else:
             logger.info('returning weather from the file')
             return weather_data
-    if weather_data != ['Weather data N/A']:
+    if weather_data != [NO_WEATHER_DATA_MESSAGE]:
         save_weather_to_file(weather_data, weather_file)
         logger.info('returning updated weather')
         return weather_data
     logger.error('Something went badly wrong')
-    return ['Weather data N/A']
+    return [NO_WEATHER_DATA_MESSAGE]
 
 
-def cleanup_weather_data(weather: str) -> list:
+def __is_weather_data_expired(date: str) -> bool:
+    last_check = date.split('-')
+    return app_timer.is_time_to_run_every_6_hours(datetime(int(last_check[0]), int(last_check[1]), int(last_check[2]),
+                                                           int(last_check[3]), int(last_check[4]), int(last_check[5])))
+
+
+def __cleanup_weather_data(weather: str) -> list:
     result = weather.split(';')
     result_list = []
-    for x in result:
-        y = x.split('.')
-        for z in y:
-            z = z.replace("\n"," ").replace('Maximum ', 'Max').replace('Minimum ', 'Min').replace('temperature', ' temp.').replace(
+    for lines in result:
+        line = lines.split('.')
+        for z in line:
+            z = z.replace("\n", " ").replace('Maximum ', 'Max').replace('Minimum ', 'Min').replace('temperature',
+                                                                                                   ' temp.').replace(
                 'daytime ', '').replace('nighttime ', '').replace('degrees Celsius', '°C')
             if z:
                 result_list.append(z.strip())
     result_list.remove('Today')
     return result_list
-
