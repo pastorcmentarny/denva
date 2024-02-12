4256935171d31e196d03ccd09a91a45cd1d77913
Dominik Symonowicz <dmspastor@gmail.com>
2024-02-12 10:59:59 +0000

    replace file based db for status with sqlite3

---
+++ src/db/db_creator.py
@@ -0,0 +1,99 @@
+import sqlite3
+from datetime import datetime
+
+
+
+default_hc = {
+    "denva_app": "20201212201221",
+    "denva_ui": "20201212201221",
+    "denva_device": 'OFF',
+    "denva2_ui": "20201212201221",
+    "denva2_motion": "20201212201221",
+    "denva2_gps": "20201212201221",
+    "denva2_barometric": "20201212201221",
+    "denva2_spectrometer": "20201212201221",
+    "denva2_sound": "20201212201221",
+    "denva2_device": 'OFF',
+    "server_app": "20201212201221",
+    "server_ui": "20201212201221",
+    "server_device": 'OFF',
+    "cctv": "20201212201221",
+    "knyszogar_hc": "20201212201221",
+    "knyszogar_radar": "20201212201221",
+    "knyszogar_digest": "20201212201221",
+    "knyszogar_app": "20201212201221",
+    "knyszogar_email": "20201212201221"
+}
+
+def count_size():
+    connection = sqlite3.connect('/home/pi/denva.db')
+    table_name = 'service_status'
+    cursor = connection.cursor()
+    query = f"SELECT COUNT(*) FROM {table_name}"
+    cursor.execute(query)
+    result = cursor.fetchone()
+    row_count = result[0]
+    print(f"ROW COUNT: {row_count}")
+    cursor.close()
+    connection.close()
+
+
+def setup():
+    connection = sqlite3.connect('/home/pi/denva.db')
+    cursor = connection.cursor()
+
+
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='service_status'")
+    cursor.execute("CREATE TABLE service_status (service TEXT, last_updated TEXT)")
+
+    for key in default_hc.keys():
+        if key.endswith('_device'):
+            params_data = [key, 'OFF']
+            cursor.execute("INSERT INTO service_status VALUES (?, ?)", params_data)
+        else:
+            params_data = [key, '20201212201221']
+            cursor.execute("INSERT INTO service_status VALUES (?, ?)", params_data)
+
+    test1 = 'denva_app'
+    rows1 = cursor.execute("SELECT service, last_updated FROM service_status WHERE service = ?",
+                          (test1,), ).fetchall()
+    test2 = 'denva_device'
+    rows2 = cursor.execute("SELECT service, last_updated FROM service_status WHERE service = ?",
+                          (test2,), ).fetchall()
+
+    test3 = 'denva_app'
+    now = datetime.now()
+    current_data = str('2021{:02d}{:02d}{:02d}{:02d}{:02d}'
+                       .format( now.month, now.day, now.hour, now.minute, now.second))
+    cursor.execute("UPDATE service_status SET last_updated = ? WHERE service = ?",
+                   (current_data, 'denva_app'))
+    rows3 = cursor.execute("SELECT service, last_updated FROM service_status WHERE service = ?",
+                          (test1,), ).fetchall()
+
+    test4 = 'denva_device'
+    current_state = 'ON'
+    cursor.execute("UPDATE service_status SET last_updated = ? WHERE service = ?",
+                   (current_state, 'denva_device'))
+    rows4 = cursor.execute("SELECT service, last_updated FROM service_status WHERE service = ?",
+                          (test2,), ).fetchall()
+    print(rows1)
+    print(rows2)
+    print(rows3)
+    print(rows4)
+    query = f"SELECT COUNT(*) FROM {table_name}"
+    cursor.execute(query)
+    result = cursor.fetchone()
+    row_count = result[0]
+    print(f"ROW COUNT: {row_count}")
+    cursor.execute('SELECT SQLITE_VERSION()')
+    data = cursor.fetchone()
+    print('SQLite version:', data)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+
+if __name__ == '__main__':
+    setup()
+    count_size()
---
+++ src/db/db_service.py
@@ -0,0 +1,109 @@
+import sqlite3
+import logging
+from datetime import datetime
+from contextlib import closing
+
+logger = logging.getLogger('app')
+
+# constants
+DB_NAME = "/home/pi/knyszogar/db/denva.db"
+STATUS_ROWS = 19
+SERVICE_TABLE = 'service_status'
+
+
+def update_for(what: str):
+    try:
+        if what.endswith('_device'):
+            logger.warning(f'Unable to update for {what}')
+            return
+        with closing(sqlite3.connect(DB_NAME)) as connection:
+            with closing(connection.cursor()) as cursor:
+                now = datetime.now()
+                current_data = str('{}{:02d}{:02d}{:02d}{:02d}{:02d}'
+                                   .format(now.year, now.month, now.day, now.hour, now.minute, now.second))
+
+                cursor.execute("UPDATE service_status SET last_updated = ? WHERE service = ?", (current_data, what))
+            logger.debug(f'Total changes :{connection.total_changes}')
+            connection.commit()
+            logger.debug(f'{what} updated to {current_data}')
+    except sqlite3.Error as error:
+        logger.error(f"Didn't get status for {what}  due to database error {error}", exc_info=True)
+    except Exception as exception:
+        logger.error(f"Didn't get update status for {what} due to exception {exception}", exc_info=True)
+
+
+def get_status_for(what: str):
+    logger.debug(f'Getting status (last updated) for {what}')
+    try:
+        with closing(sqlite3.connect(DB_NAME)) as connection:
+            with closing(connection.cursor()) as cursor:
+                logger.debug(f'Total changes before:{connection.total_changes}')
+                result = cursor.execute("SELECT service, last_updated FROM service_status WHERE service = ?",
+                                        (what,)).fetchall()
+                logger.warning(f'Status for {what} is {result}')
+                return result[1]
+            logger.debug(f'Total changes after:{connection.total_changes}')
+    except sqlite3.Error as error:
+        logger.error(f"Didn't get status for {what}  due to database error {error}",
+                     exc_info=True)
+    except Exception as exception:
+        logger.error(f"Didn't get update status for {what} due to exception {exception}",
+                     exc_info=True)
+
+
+def set_device_on(device_name: str):
+    if device_name.endswith("_device"):
+        set_device_status_to(device_name, "OK")
+    else:
+        print(f'Unable to update for {device_name}')
+
+
+def set_device_off(device_name: str):
+    if device_name.endswith("_device"):
+        set_device_status_to(device_name, "OFF")
+    else:
+        print(f'Unable to update for {device_name}')
+
+
+def set_device_status_to(device_name: str, device_state: str):
+    try:
+        with closing(sqlite3.connect(DB_NAME)) as connection:
+            with closing(connection.cursor()) as cursor:
+                cursor.execute("UPDATE service_status SET last_updated = ? WHERE service = ?",
+                               (device_state, device_name))
+                print(connection.total_changes)
+            print(f'Total changes :{connection.total_changes}')
+            connection.commit()
+    except sqlite3.Error as error:
+        logger.error(f"Didn't update status for {device_name} to {device_state} due to database error {error}",
+                     exc_info=True)
+    except Exception as exception:
+        logger.error(f"Didn't update status for {device_name} to {device_state} due to exception {exception}",
+                     exc_info=True)
+
+
+def self_test():
+    try:
+        print('TEST started')
+        with closing(sqlite3.connect(DB_NAME)) as connection:
+            with closing(connection.cursor()) as cursor:
+                rows = cursor.execute("SELECT 1").fetchall()
+                print(f'Test result: {rows}')
+                table_name = 'service_status'
+                query = f"SELECT COUNT(*) FROM {table_name}"
+                cursor.execute(query)
+                result = cursor.fetchone()
+                row_count = result[0]
+                if int(row_count) == STATUS_ROWS:
+                    logger.info(f'Row count is correct {row_count}')
+                else:
+                    logger.warning(f'It should be {STATUS_ROWS} rows but there are {row_count}')
+        print('TEST stopped')
+    except sqlite3.Error as error:
+        logger.error(f"During test connection database attacked with error {error}", exc_info=True)
+    except Exception as exception:
+        logger.error(f"During test connection application blew up with exception {exception}", exc_info=True)
+
+
+if __name__ == '__main__':
+    self_test()
